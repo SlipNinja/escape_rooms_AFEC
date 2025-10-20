1653771b0612658616a4755dba29a17ebd7fc47e
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2025-10-20 10:35:18 +0200

    add roommanager

--- RoomManager.js
+++ RoomManager.js
@@ -1,36 +1,19 @@
 class RoomManager {
 	container = document.getElementById("main_container");
 
-	load_room(room) {
-		let room_data = this.fetch_room();
-		room_data.parent = this.container;
+	async load_room(room_number) {
+		let room_data = await this.fetch_room(room_number);
+		console.log("ROOM DATA", typeof room_data, room_data);
+		this.container.innerHTML = room_data;
 	}
 
-	fetch_room() {
-		fetch("./rooms/room_1/room_1.html")
-			.then((response) => {
-				// When the page is loaded convert it to text
-				return response.text();
-			})
-			.then((html) => {
-				// Initialize the DOM parser
-				const parser = new DOMParser();
-
-				// Parse the text
-				const doc = parser.parseFromString(html, "text/html");
-
-				// You can now even select part of that html as you would in the regular DOM
-				// Example:
-				// const docArticle = doc.querySelector('article').innerHTML
-
-				console.log(doc);
-				console.log(doc.querySelector(".content"));
-			})
-			.catch((error) => {
-				console.error("Failed to fetch page: ", error);
-			});
+	async fetch_room(room) {
+		const html_file = `./rooms/room_${room}/room_${room}.html`;
+		return fetch(html_file).then((response) => {
+			return response.text();
+		});
 	}
 }
 
 let Manager = new RoomManager();
-Manager.load_room();
+Manager.load_room(1);
