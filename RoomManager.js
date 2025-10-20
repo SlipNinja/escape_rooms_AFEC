2e802c327f36c841be61bf913f57e9e876bf37d7
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2025-10-20 10:47:32 +0200

    add css replacement

--- RoomManager.js
+++ RoomManager.js
@@ -2,16 +2,22 @@ class RoomManager {
 	container = document.getElementById("main_container");
 
 	async load_room(room_number) {
-		let room_data = await this.fetch_room(room_number);
-		console.log("ROOM DATA", typeof room_data, room_data);
+		let room_data = await this.fetch_room_html(room_number);
 		this.container.innerHTML = room_data;
+		this.replace_css(room_number);
 	}
 
-	async fetch_room(room) {
+	async fetch_room_html(room) {
 		const html_file = `./rooms/room_${room}/room_${room}.html`;
 		return fetch(html_file).then((response) => {
 			return response.text();
 		});
+	}
+
+	replace_css(room) {
+		const html_file = `./rooms/room_${room}/room_${room}.css`;
+		const css_link = document.getElementsByTagName("link")[0];
+		css_link.href = html_file;
 	}
 }
 
