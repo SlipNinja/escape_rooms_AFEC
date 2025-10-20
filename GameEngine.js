d1ac2b43a42bf4baa99cadfe2f69ec6ee4d2a63c
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2025-10-20 14:08:10 +0200

    refactor async get questions

--- GameEngine.js
+++ GameEngine.js
@@ -2,25 +2,10 @@ class GameEngine {
 	roomNumber = 1;
 	questions = null;
 
-	constructor() {}
-
-	async getRoomQuestion() {
-		return fetch(`rooms/room_${this.roomNumber}/questions.json`).then((r) => {
-			if (r.ok) {
-				console.log("Get Questions");
-				return r.json();
-			} else {
-				throw new Error("Erreur serveur", { cause: r });
-			}
-		});
-	}
-
 	async checkAnswer(val) {
-		console.log("Check answers");
-		return this.getRoomQuestion().then((response) => {
-			this.questions = response;
-			console.log("CHECK QUESTION ", this.questions);
-			return val == this.questions.answer;
-		});
+		const response = await fetch(`rooms/room_${this.roomNumber}/questions.json`);
+		const data = await response.json();
+		console.log("DATA ", data);
+		return val == data.answer;
 	}
 }
