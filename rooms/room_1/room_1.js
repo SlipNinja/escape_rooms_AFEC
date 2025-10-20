4e8fad72a1d3ce2eaab2651012cf93b770a26ff7
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2025-10-20 14:50:34 +0200

    added room 1

--- rooms/room_1/room_1.js
+++ rooms/room_1/room_1.js
@@ -1,9 +1,18 @@
+const btns = [];
+
 function add_btn() {
-	const new_button = document.createElement("button");
-	new_button.textContent = "I'm a new button !";
-	const container = document.getElementById("main_container");
-	container.appendChild(new_button);
-	console.log("BUTTON ADDED");
+	if (btns.length < 60) {
+		const new_button = document.createElement("button");
+		new_button.textContent = "Try me, duh";
+		new_button.onclick = arguments.callee;
+		const container = document.querySelector(".room");
+		container.appendChild(new_button);
+		btns.push(new_button);
+		if (btns.length == 36) {
+			new_button.textContent = "St4ckOv3rfl0w";
+			new_button.id = "answer";
+		}
+	}
 }
 
 add_btn();
