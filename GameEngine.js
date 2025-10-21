34e0cd0047cf8013a5d766daf9c691b070ef3e59
Marie <marie.nahapetian@gmail.com>
2025-10-21 10:13:43 +0200

    changes to gameEngine class to use only 1 button click

--- GameEngine.js
+++ GameEngine.js
@@ -18,6 +18,14 @@ class GameEngine {
 		return answer == data.answer;
 	}
 
+	async getQuestion() {
+		let currentRoom = this.room.get_current_room();
+		const response = await fetch(`rooms/room_${currentRoom}/questions.json`);
+		const data = await response.json();
+
+		return data;
+	}
+
 	update() {
 		let _this = this;
 		this.checkAnswer().then(function (response) {
@@ -40,39 +48,60 @@ class GameEngine {
 		document.getElementById("answer").value = "";
 	}
 
-	startGame() {
-		document.getElementsByClassName("answer")[0].style.display = "flex";
+	// startGame() {
+	// 	document.getElementsByClassName("answer")[0].style.display = "flex";
 
-		this.room.increment_room();
-		this.room.load_room(this.room.get_current_room());
-	}
+	// 	this.room.increment_room();
+	// 	this.room.load_room(this.room.get_current_room());
+	// }
 
 	nextPage() {
-		this.validateInput();
-		this.room.increment_room();
-		this.room.load_room(this.room.get_current_room());
+		//debugger;
+		if (this.room.get_current_room()) {
+			let _this = this;
+			this.checkAnswer().then(function (response) {
+				if (response) {
+					_this.validateInput();
+					_this.room.increment_room();
+					_this.room.load_room(_this.room.get_current_room());
+				} else {
+					_this.displayErrorMessage();
+				}
+			});
+		} else {
+			this.room.increment_room();
+			this.room.load_room(this.room.get_current_room());
+			document.getElementsByClassName("answer")[0].style.display = "flex";
+		}
 	}
 }
 
 const gameEngine = new GameEngine();
-const main_button = document.getElementById("main_container").firstElementChild;
-const next_button = document.getElementsByClassName("next_btn")[0];
-document.getElementsByClassName("answer")[0].style.display = "none";
+//const main_button = document.getElementById("main_container").firstElementChild;
+const next_buttons = document.getElementsByClassName("next_btn");
+//document.getElementsByClassName("answer")[0].style.display = "none";
 
-// Main button
-main_button.addEventListener("click", function (e) {
-	gameEngine.startGame();
-});
+// // Main button
+// main_button.addEventListener("click", function (e) {
+// 	gameEngine.startGame();
+// });
 
 // Validation button
-next_button.addEventListener("click", function (e) {
-	gameEngine.checkAnswer().then(function (response) {
-		if (response) {
-			gameEngine.validateInput();
-			gameEngine.room.increment_room();
-			gameEngine.room.load_room(gameEngine.room.get_current_room());
-		} else {
-			gameEngine.displayErrorMessage();
-		}
+
+for (var i = 0; i < next_buttons.length; i++) {
+	next_buttons[i].addEventListener("click", function (e) {
+		gameEngine.nextPage();
 	});
-});
+}
+// next_button.addEventListener("click", function (e) {
+// 	gameEngine.nextPage();
+// 	// gameEngine.checkAnswer().then(function (response) {
+// 	// 	if (response) {
+// 	// 		gameEngine.validateInput();
+// 	// 		gameEngine.room.increment_room();
+// 	// 		gameEngine.room.load_room(gameEngine.room.get_current_room());
+// 	// 	} else {
+// 	// 		gameEngine.displayErrorMessage();
+// 	// 	}
+// 	// });
+// });
